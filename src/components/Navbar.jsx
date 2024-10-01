13c3aee89a0eb7227e11a9b5a82dccafbdb1fea8
x <sonulinux007x@gmail.com>
2024-10-01 23:30:28 +0530

    6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,18 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+        <div className="flex h-16 bg-yellow-200 justify-center items-center text-center">
           <div className="flex">
-            <div className="flex-shrink-0">
-              <h1 className="text-2xl font-bold text-gray-800"><a href="/">X</a></h1>
-            </div>
+            
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
               <Link 
                 to="/myself"
                 className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
                 Myself
               </Link>
+              <div className="flex-shrink-0">
+              <h1 className="text-2xl font-bold text-gray-800 "><a href="/">[X]</a></h1>
+            </div>
               <Link 
                 to="/blog"
                 className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
