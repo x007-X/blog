c707b640e09a8eba3421051318c5d301a976bff0
x <sonulinux007x@gmail.com>
2024-10-01 18:29:54 +0530

    3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Blog from './pages/Blog';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className=" bg-gray-100">
         <Navbar />
         <div className="p-8">
           <Routes>
@@ -16,7 +16,14 @@ function App() {
             <Route path="/blog" element={<Blog />} />
           </Routes>
         </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-0 w-[500px] h-[500px] items-center justify-center">
+  <img src="/a.jpg" alt="Image 1" className="h-full w-full object-cover" />
+  <img src="/b.jpg" alt="Image 2" className="h-full w-full object-cover" />
+  <img src="/c.jpg" alt="Image 3" className="h-full w-full object-cover" />
+  <img src="/e.jpg" alt="Image 4" className="h-full w-full object-cover" />
+</div>
       </div>
+      
     </Router>
   );
 }
