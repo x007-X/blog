7e50c20f42609e2c31ceefb5c90cec563e627c22
x <sonulinux007x@gmail.com>
2024-10-04 22:27:38 +0530

    19

---
+++ src/pages/BlogPost1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPost1 = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Blog Post 1</h1>
+      <p>This is the content of Blog Post 1.</p>
+    </div>
+  );
+};
+
+export default BlogPost1;
