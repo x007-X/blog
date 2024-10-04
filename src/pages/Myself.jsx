354a7e9357bbf90f6131c5b3902d938a3e87c11a
x <sonulinux007x@gmail.com>
2024-10-04 22:58:26 +0530

    23

--- src/pages/Myself.jsx
+++ src/pages/Myself.jsx
@@ -17,7 +17,7 @@ const Myself = () => {
     <div>
       <h2 className="text-3xl font-bold text-gray-800">About Myself</h2>
       <p className="mt-4 text-gray-600">
-        I walk a path where the boundaries between technology and human thought blur. The lure of AGI, the mysteries of space, and the promise of fusion energy—these fascinate me not because of their finality, but because they represent the unknown, the unconquered. My work in web development is, at its core, an extension of this desire to understand, to create meaning in a world driven by logic yet guided by emotion. Through code, I build not just applications, but windows into the mind.
+        I walk a path where the boundaries between technology and human thought blur.
       </p>
       <p className="mt-4 text-gray-600">
         The lure of AGI, the mysteries of space, and the promise of fusion energy—these fascinate me not because of their finality, but because they represent the unknown, the unconquered.
@@ -29,7 +29,7 @@ const Myself = () => {
       Through code, I build not just applications, but windows into the mind.
       </p>
       {/* Slideshow Component */}
-      <div className="slideshow-container relative w-[800px] h-[400px] mx-auto overflow-hidden my-6 rounded-3xl">
+      <div className="slideshow-container relative w-[800px] h-[400px] mx-auto overflow-hidden my-6 rounded-3xl mt-10">
         {slides.map((slide, index) => (
           <div
             key={index}
@@ -43,17 +43,19 @@ const Myself = () => {
       </div>
 
       <h2 className="mt-6 text-2xl font-semibold text-gray-800">The Instruments of My Craft</h2>
-      <ul className="mt-4 text-gray-600 list-inside space-y-1 pl-4">
-        <li>🐍 Python</li>
-        <li>💻 C</li>
-        <li>⚛️ React</li>
-        <li>🌐 HTML</li>
-        <li>🎨 CSS</li>
-        <li>✨ JavaScript</li>
-        <li>🔗 Next.js</li>
-        <li>🖼️ Vue</li>
-        <li>🛢️ MySQL</li>
-      </ul>
+      <ul className="mt-10 text-gray-600 list-inside pl-4 flex justify-center items-center flex-wrap space-x-4 text-center">
+  <li>🐍 Python</li>
+  <li>💻 C</li>
+  <li>⚛️ React</li>
+  <li>🌐 HTML</li>
+  <li>🎨 CSS</li>
+  <li>✨ JavaScript</li>
+  <li>🔗 Next.js</li>
+  <li>🖼️ Vue</li>
+  <li>🛢️ MySQL</li>
+</ul>
+
+
     </div>
   );
 };
