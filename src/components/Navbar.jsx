f9328475be8cb14d9e6a845761327963f66cc67a
x <sonulinux007x@gmail.com>
2024-10-11 18:35:06 +0530

    55

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 justify-center items-center">
-          <div className="flex items-center space-x-14">
+          <div className="flex items-center space-x-16">
             <Link 
               to="/myself"
               className="inline-flex items-center text-sm font-medium text-gray-600 hover:text-gray-700 transition duration-200"
